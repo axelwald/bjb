03330262ef52892b8e9fbb0e3b069dabc2375d3f
Axel Waldenborg <axel.waldenborg@learnet.se>
2024-04-22 14:53:31 +0200

    jaoieujfojiaef

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
   "./index.html"],
   theme: {
-    
     extend: {
+
     },
-  },
-  plugins: [],
+
+  }
+
 }
 
