4bc9c4fb08a3023e2f63fbdc9ee2639b8a6ab5b3
AlvinGranvik <alvin.granvik@elev.ga.ntig.se>
2024-05-19 16:42:04 +0200

    Baba Ganoush

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  "./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'kanit': ['kanit-regular, Kanit, sans-serif']
+    }
+
+  }
+
+}
+
